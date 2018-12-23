ad51fa254a4b09f79ed980598e40691d660fc3a0
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-12-24 01:29:31 +0300

    Updated tests

--- run_tests.sh
+++ run_tests.sh
@@ -19,4 +19,7 @@ python $exe -t users.sudo_access.yml    -v vars.nosudouser1.yml   -e host=$targe
 python $exe -t users.sudo_access.yml    -v vars.admin.yml         -e host=$target_host -e rc_pass=1
 #
 python $exe -t sys.network.yml          -v vars.user1.yml         -e host=$target_host
+python $exe -t sys.internet.yml         -v vars.user1.yml         -e host=$target_host
+python $exe -t sys.internet.yml         -v vars.nosudouser1.yml   -e host=$target_host
+python $exe -t sys.internet.yml         -v vars.admin.yml         -e host=$target_host
 
